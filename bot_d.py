6124dc07bc4214db0bf480a5a7c61f6f2ef48e6a
booshington <kyleisagod@gmail.com>
2019-06-17 19:14:06 -0400

    basic bot functionality, responds to simple statement with acknowledgement

--- bot_d.py
+++ bot_d.py
@@ -1,12 +1,36 @@
 from slackclient import SlackClient
-import os
+import os, time
 
+#retrieves token, creates slack client
 f = open("oauth","r")
 token=f.readline().splitlines()[0]
 sc = SlackClient(token)
 
 
-#connects with token
-print("hey")
-if sc.rtm_connect(with_team_state=False):
-    print("also hey")
+def main():
+    #connects with token
+    if not sc.rtm_connect():
+        print("error in authentication")
+        return
+    
+    #sends a message to the channel tha the bot is active
+    sc.api_call("chat.postMessage",channel="chatbots", text="G.O.L.I.A.T.H. oNLINE")
+
+    #bot loop. for events, process the text and send a response, if any
+    while True:
+        events = sc.rtm_read()
+        for event in events:
+            if event["type"] == "message":
+                botResponse(event["text"])
+        time.sleep(1)
+
+#bot responses and logic is stored here.
+def botResponse(text):
+    if "magic4321" in text:
+        return sendBotResponse(1)
+
+def sendBotResponse(resp):
+    if resp == 1:
+        sc.api_call("chat.postMessage",channel="chatbots", text="You said the magic word!")
+
+main()
